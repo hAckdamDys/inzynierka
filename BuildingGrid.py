823bb94fe719439d71bd0ef42f81449659e6d8f8
Adam Dyszy <electro.ubro@gmail.com>
2019-01-08 02:06:24 +0100

    added moving robot function

--- BuildingGrid.py
+++ BuildingGrid.py
@@ -1,6 +1,7 @@
 import numpy as np
+import Robot
 
-# grid corresponding to actuall map where robots move and put blocks
+# grid corresponding to actual map where robots move and put blocks
 class BuildingGrid:
     def __init__(self, width, height) -> None:
         super().__init__()
@@ -8,20 +9,36 @@ class BuildingGrid:
         self.height = height
         # grid will have 0 if nothing is on given tile
         # and index of tile otherwise
-        self.grid = np.zeros((width,height),int)
-        self.tiles = [dict(),dict()]
+        self.tileGrid = np.zeros((width,height),int)
+        self.tilesFromIndex = dict()
+        self.indexFromTiles = dict()
         self.lastTileIndex = 0
         # robots are on separate layer on grid to simplify things
-        self.robots = [dict(),dict()]
-        self.lastRobotIndex=0
+        # since robots are unique we dont need indexes
+        self.robotsGrid = np.zeros((width,height),dtype=Robot)
+        self.positionFromRobot = dict()
 
-    def addTile(self, tile):
+    def addNewTile(self, tile):
         self.lastTileIndex += 1
-        self.tiles[0][self.lastTileIndex] = tile
-        self.tiles[1][tile] = self.lastTileIndex
+        self.tilesFromIndex[self.lastTileIndex] = tile
+        self.indexFromTiles[tile] = self.lastTileIndex
+
+    def addTile(self, tile, x, y):
+        tileIndex=self.indexFromTiles[tile]
+        self.tileGrid[x,y]=tileIndex
 
     def addRobot(self, robot):
-        self.lastRobotIndex += 1
-        self.robots[0][self.lastRobotIndex] = robot
-        self.robots[1][robot] = self.lastRobotIndex
+        self.robotsGrid[robot.x,robot.y] = robot
+        self.positionFromRobot[robot] = (robot.x,robot.y)
 
+    def moveRobot(self, robot, nextX, nextY):
+        # TODO: implement this function
+        canMove = self.checkIfCanMove(robot)
+        if not canMove:
+            return
+        # clean last position
+        self.robotsGrid[robot.x, robot.y] = 0
+        # update new position
+        self.robotsGrid[nextX, nextY] = robot
+        self.positionFromRobot[robot] = (nextX, nextY)
+        robot.updatePosition(nextX,nextY)
