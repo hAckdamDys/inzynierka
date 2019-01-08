92c2385323c7aea0026aa2472ea1b0339fb01152
Adam Dyszy <electro.ubro@gmail.com>
2019-01-08 01:05:16 +0100

    fixing building grid to have separate layer for robots

--- BuildingGrid.py
+++ BuildingGrid.py
@@ -10,9 +10,18 @@ class BuildingGrid:
         # and index of tile otherwise
         self.grid = np.zeros((width,height),int)
         self.tiles = [dict(),dict()]
-        self.lastTileIndex=0
+        self.lastTileIndex = 0
+        # robots are on separate layer on grid to simplify things
+        self.robots = [dict(),dict()]
+        self.lastRobotIndex=0
 
     def addTile(self, tile):
         self.lastTileIndex += 1
         self.tiles[0][self.lastTileIndex] = tile
         self.tiles[1][tile] = self.lastTileIndex
+
+    def addRobot(self, robot):
+        self.lastRobotIndex += 1
+        self.robots[0][self.lastRobotIndex] = robot
+        self.robots[1][robot] = self.lastRobotIndex
+
