bf2bc31e34bb69040755696f4b4c2fd2867f1ce8
Adam Dyszy <electro.ubro@gmail.com>
2019-01-17 04:26:51 +0100

    add add test case with wrong grid

--- Tests/test_goalBuilding.py
+++ Tests/test_goalBuilding.py
@@ -6,8 +6,8 @@ from SwarmBots.GoalBuilding import GoalBuilding
 class TestGoalBuilding(unittest.TestCase):
     """Basic test cases."""
 
-    def test_simple_grid(self):
-        textGrid = """
+    def setUp(self):
+        self.textGrid = """
         0 0 0 0 0 0 0
         0 0 0 1 0 0 0
         0 0 1 2 1 0 0
@@ -16,7 +16,7 @@ class TestGoalBuilding(unittest.TestCase):
         0 0 0 1 0 0 0
         0 0 0 0 0 0 0
         """
-        resultGrid = np.array([
+        self.resultGrid = np.array([
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
             [0, 0, 1, 2, 1, 0, 0],
@@ -25,9 +25,24 @@ class TestGoalBuilding(unittest.TestCase):
             [0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0]
         ])
+
+    def test_simple_grid(self):
+        textGrid = self.textGrid
+        resultGrid = self.resultGrid
         goalBuilding = GoalBuilding(textGrid)
         assert np.array_equal(goalBuilding.grid, resultGrid)
 
+    def test_wrong_grid(self):
+        textGrid = """
+        0 0 0 0 0 0 0
+        0 0 0 1 0 0 0
+        0 0 1 2 1 0 0
+        0 1 2 3 2 1 0
+        0 0 1 2 1 0 0
+        0 0 0 1 0 0 0
+        0 0 0 0 0 0
+        """
+        self.assertRaises(ValueError, GoalBuilding, textGrid)
 
 if __name__ == '__main__':
     unittest.main()
