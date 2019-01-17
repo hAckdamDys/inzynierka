4a899e9a3d0c6cad33ddf0c04beeb5a2d1a1472f
Adam Dyszy <electro.ubro@gmail.com>
2019-01-17 04:12:44 +0100

    change the use of matrix to another method

--- SwarmBots/GoalBuilding.py
+++ SwarmBots/GoalBuilding.py
@@ -8,31 +8,12 @@ class GoalBuilding:
         self.parseTextGrid(textGrid)
 
     def parseTextGrid(self, textGrid):
-        textGrid=";".join([s.lstrip(' ').rstrip(' ') for s in textGrid.splitlines() if s]).lstrip(';').rstrip(';')
-        textGrid=' '.join(textGrid.split())
-        textGrid=textGrid.replace(' ',',')
-        self.grid=np.array(np.matrix(textGrid))
-        self.height=self.grid.shape[0]
-        self.width=self.grid.shape[1]
-
-## TODO: add test to check for example this:
-# exampleTextGrid="""
-# 0 0 0 0 0 0 0
-# 0 0 0 1 0 0 0
-# 0 0 1 2 1 0 0
-# 0 1 2 3 2 1 0
-# 0 0 1 2 1 0 0
-# 0 0 0 1 0 0 0
-# 0 0 0 0 0 0 0
-# """
-
-# to this:
-# np.array([[0, 0, 0, 0, 0, 0, 0],
-#        [0, 0, 0, 1, 0, 0, 0],
-#        [0, 0, 1, 2, 1, 0, 0],
-#        [0, 1, 2, 3, 2, 1, 0],
-#        [0, 0, 1, 2, 1, 0, 0],
-#        [0, 0, 0, 1, 0, 0, 0],
-#        [0, 0, 0, 0, 0, 0, 0]])
-
-# and maybe some others
+        rowSep = ';'
+        textGridTmp = textGrid.replace(os.linesep, rowSep)
+        textGridTmp = textGridTmp.replace('\n', rowSep)
+        textGridTmp = textGridTmp.lstrip(rowSep).rstrip(rowSep)
+        self.height = textGridTmp.count(rowSep)
+        firstRow = np.fromstring(textGridTmp.split(rowSep)[0])
+        self.width = firstRow.shape[0]
+        self.grid = np.fromstring(textGrid, dtype=int, sep=' ').reshape(
+            self.height, self.width)
